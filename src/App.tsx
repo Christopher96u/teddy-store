0e2550051a662f21f29c4c0dda406ca619a8b133
Christopher <christophermenendezmorales@gmail.com>
2022-02-26 00:29:05 -0500

    feat: remove static images

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Slider from '@components/Slider/Slider';
-import image1 from './assets/slide_1.jpeg';
-import image2 from './assets/slide_2.jpeg';
-import image3 from './assets/slide_3.jpeg';
-import image4 from './assets/slide_4.jpeg';
-import image5 from './assets/slide_5.jpeg';
-import image6 from './assets/slide_6.jpeg';
 import useCollection from '@hooks/useCollection';
 
 function App() {
